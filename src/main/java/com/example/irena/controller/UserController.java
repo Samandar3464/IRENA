4b17b7a164acf58d4fd6fc9c947a6c530296bee4
Samandar3464 <107240826+Samandar3464@users.noreply.github.com>
2023-03-26 23:42:54 +0500

    base configuration 2

--- src/main/java/com/example/irena/controller/UserController.java
+++ src/main/java/com/example/irena/controller/UserController.java
@@ -1,15 +1,11 @@
 package com.example.irena.controller;
 
+import com.example.irena.entity.UserEntity;
+import com.example.irena.model.UserRegisterDTO;
+import com.example.irena.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import uz.pdp.spring_boot_security_web.entity.UserEntity;
-import uz.pdp.spring_boot_security_web.model.dto.receive.UserRegisterDTO;
-import uz.pdp.spring_boot_security_web.service.UserService;
-
-import java.io.IOException;
 
 @Controller
 @RequestMapping("/api/user")
@@ -34,16 +30,5 @@ public class UserController {
             return "redirect:/";
         }
         return "redirect:/register";
-    }
-    @GetMapping("/editPhoto")
-    public String savePage(){
-        return "cabinet";
-    }
-    @PostMapping("/save")
-    public String savePhoto(@RequestParam("file")MultipartFile file) throws IOException {
-        UserEntity userEntity= (UserEntity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        userService.editUserEntity(userEntity.getId(),file );
-
-        return "redirect:/";
     }
 }
