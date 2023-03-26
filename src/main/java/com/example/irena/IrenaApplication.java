6222e8eabc8b6f9f204880565121eda539b378fe
Samandar3464 <107240826+Samandar3464@users.noreply.github.com>
2023-03-26 14:13:55 +0500

    first commit

---
+++ src/main/java/com/example/irena/IrenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.irena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/irena/IrenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.irena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
